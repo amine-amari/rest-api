67ce79425018df846b775474ac384a700cdd7207
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-18 18:51:11 +0100

    add routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 // Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World');
+})
 // Getting One
+router.get('/:id', (req, res) => {
+    res.send(req.params.id);
+})
 // Creating One
+router.post('/', (req, res) => {
+    
+})
 // Updating one
+router.patch('/:id', (req, res) => {
+    
+})
 // Deleting One
+router.delete('/:id', (req, res) => {
+    
+})
 
 module.exports = router;
