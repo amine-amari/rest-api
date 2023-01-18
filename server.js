9b6e2c84f0559a7acd71277a3415781845cc72fb
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-18 18:24:32 +0100

    add db connection to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connect to DB'));
 
 app.listen(3000, () => console.log('Server Started'));
