0bc1f92025cfc67c507b98679a6421514b53711b
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-18 18:41:11 +0100

    add subscriber router

---
+++ routes/subscribers.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Getting all
+// Getting One
+// Creating One
+// Updating one
+// Deleting One
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,4 +10,10 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connect to DB'));
 
+
+app.use(express.json());
+
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+
 app.listen(3000, () => console.log('Server Started'));
